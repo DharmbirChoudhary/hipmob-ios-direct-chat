eacc67b9bccdb00fd5caa1d3ca33b1f5e5cbe4c0
Femi Omojola <femi@hipmob.com>
2012-09-25 02:52:17 -0400

    Initial Commit

---
+++ HipmobChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HipmobChat
+//
+//  Created by Olufemi Omojola on 9/25/12.
+//  Copyright (c) 2012 Orthogonal Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestAppDelegate class]));
+    }
+}
